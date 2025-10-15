384b71c4477134d78244c35925764fc9112a53f7
JorbFreire <jorbfreire@gmail.com>
2025-10-15 03:25:31 -0300

    fix debounce

--- data-view/data_view/public/js/basicPlot/loadPythonBridge.js
+++ data-view/data_view/public/js/basicPlot/loadPythonBridge.js
@@ -7,16 +7,27 @@ function debouncedPythonBridge(newValues) {
   if (bridgeDebounceTimeoutId)
     clearTimeout(bridgeDebounceTimeoutId)
 
-  bridgeDebounceTimeoutId = setTimeout(() => {
+  const execute = () => {
     const mergedNewValues = {
       ...newValues,
       ...debounceWaitingNewValues
     }
+    debounceWaitingNewValues = mergedNewValues
     loadPythonBridge(mergedNewValues)
+  }
+
+  const hasToggleAction = Object.keys(newValues).some(key => key.includes("toggle"))
+
+  if (hasToggleAction)
+    execute()
+
+  bridgeDebounceTimeoutId = setTimeout(() => {
+    execute()
   }, DEBOUNCE_TIME_IN_MS)
 }
 
 function loadPythonBridge(newValues) {
+  debounceWaitingNewValues = {}
   const bokehDocument = Bokeh.documents[0]
   const update_plot_options_trigger = bokehDocument.get_model_by_name(
     "update_plot_options_trigger"
@@ -31,6 +42,4 @@ function loadPythonBridge(newValues) {
   )
 
   update_plot_options_trigger.data = { ...newValuesWrapped }
-
-  debouncedPythonBridge(newValues)
 }
